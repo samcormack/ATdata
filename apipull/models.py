3b1a0a3b32584a6322c164c4a9c75825770daa4b
sam <sam.cormack@gmail.com>
2017-08-30 14:19:20 +1200

    API calls working.

--- apipull/models.py
+++ apipull/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class RTentry(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now)
+    id_no = models.CharField(max_length=100)
+    trip_id = models.CharField(max_length=100)
+    route_id = models.CharField(max_length=100)
+    stop_seq = models.IntegerField()
+    stop_id = models.CharField(max_length=20)
+    stop_type = models.CharField(max_length=20)
+    vehicle_id = models.CharField(max_length=20)
+    delay = models.IntegerField()
+    time = models.DateTimeField()
+
+    def __str__(self):
+        return self.id_no
